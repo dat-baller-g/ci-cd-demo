6655c4170e94a99612295487c19a0e2c1d241a50
Mobolaji <ajakaiye@systemspecs.com.ng>
2025-12-19 13:46:21 +0100

    updated code to allow jest exit after test

--- app.test.js
+++ app.test.js
@@ -1,9 +1,18 @@
 const request = require('supertest');
 const app = require('./index');
+let server;
+
+beforeAll(() => {
+  server = require("./index");
+});
 
 describe('Health check', () => {
   it('should return OK', async () => {
     const res = await request(app).get('/health');
     expect(res.statusCode).toBe(200);
   });
+});
+
+afterAll((done) => {
+  server.close(done);
 });
--- index.js
+++ index.js
@@ -15,4 +15,4 @@ app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
 
-module.exports = app;
+module.exports = server;
