4c473f278d8dae4bb0319ad299cf161651e56c17
Mobolaji <ajakaiye@systemspecs.com.ng>
2025-12-19 16:41:39 +0100

    Confirm CI/CD pipeline is working as it should

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'CI/CD Pipeline is working' });
+  res.json({ message: 'CI/CD Pipeline is working. I just confirmed it!' });
 });
 
 app.get('/health', (req, res) => {
